3ebe0c171df24077b0588176216d4756051d92b9
rmfabac <rmfabac047@gmail.com>
2017-03-07 16:10:58 -0600

    Update CAN Frame Id's
    Updated to hex representation (unsigned), instead of decimal.

--- e46canbusframe.h
+++ e46canbusframe.h
@@ -17,11 +17,11 @@ class E46CanBusFrame : public QCanBusFrame
         short decodeOilTempC(const QByteArray&) const;
         short decodeOilTempF(const QByteArray&) const;
     private:
-        const unsigned short engine_rpm_id_ = 790;
+        const unsigned short engine_rpm_id_ = 0x316u;
         const unsigned short vehicle_speed_id_ = 0; // Value still needed.
-        const unsigned short fuel_level_id_ = 1555;
-        const unsigned short coolant_temp_c_id_ = 809;
-        const unsigned short oil_temp_c_id_ = 1349;
+        const unsigned short fuel_level_id_ = 0x613u;
+        const unsigned short coolant_temp_c_id_ = 0x329u;
+        const unsigned short oil_temp_c_id_ = 0x545u;
 };
 
 #endif // E46CANBUSFRAME_H
